b1ee70c2b41cb8990647973a5f281968c3cae924
JamesDeGrizz <xoce90@mail.ru>
2020-09-02 00:29:58 +0300

    wip #20

--- src/main/java/chat/src/main/java/client/Controller.java
+++ src/main/java/chat/src/main/java/client/Controller.java
@@ -12,190 +12,258 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML
-    MenuBar menubar;
+	@FXML
+	MenuBar menubar;
 
-    @FXML
-    TextArea textArea;
+	@FXML
+	TextArea textArea;
 
-    @FXML
-    TextField msgField, loginField;
+	@FXML
+	TextField msgField, loginField;
 
-    @FXML
-    HBox msgPanel, authPanel, infoPanel, btnPanel;
+	@FXML
+	HBox msgPanel, authPanel, infoPanel, btnPanel;
 
-    @FXML
-    PasswordField passField;
+	@FXML
+	PasswordField passField;
 
-    @FXML
-    ListView<String> clientsList;
+	@FXML
+	ListView<String> clientsList;
 
-    private Stage regStage;
-    RegController regController;
+	private Stage regStage;
+	RegController regController;
 
-    private Stage nickChangerStage;
-    NicknameChangerController nickChangeController;
+	private Stage nickChangerStage;
+	NicknameChangerController nickChangeController;
 
-    private Network network;
-    private String nickname;
+	private Network network;
+	private String nickname;
 
-    public void setAuthenticated(boolean authenticated) {
-        authPanel.setVisible(!authenticated);
-        authPanel.setManaged(!authenticated);
-        btnPanel.setVisible(!authenticated);
-        btnPanel.setManaged(!authenticated);
-        menubar.setVisible(authenticated);
-        menubar.setManaged(authenticated);
-        msgPanel.setVisible(authenticated);
-        msgPanel.setManaged(authenticated);
-        infoPanel.setVisible(authenticated);
-        infoPanel.setManaged(authenticated);
-        if (!authenticated) {
-            nickname = "";
-        }
-    }
+	public void setAuthenticated(boolean authenticated) {
+		authPanel.setVisible(!authenticated);
+		authPanel.setManaged(!authenticated);
+		btnPanel.setVisible(!authenticated);
+		btnPanel.setManaged(!authenticated);
+		menubar.setVisible(authenticated);
+		menubar.setManaged(authenticated);
+		msgPanel.setVisible(authenticated);
+		msgPanel.setManaged(authenticated);
+		infoPanel.setVisible(authenticated);
+		infoPanel.setManaged(authenticated);
+		if (!authenticated) {
+			nickname = "";
+		}
+	}
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        setAuthenticated(false);
-        createNetwork();
-        network.connect();
-        passField.requestFocus();
-        regStage = createRegWindow();
-        nickChangerStage = createNicknameChanger();
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setAuthenticated(false);
+		createNetwork();
+		network.connect();
+		passField.requestFocus();
+		regStage = createRegWindow();
+		nickChangerStage = createNicknameChanger();
 
-        clientsList.setOnMouseClicked(event -> {
-                if (event.getClickCount() == 2) {
-                    msgField.clear();
-                    msgField.setText("/w " + clientsList.getSelectionModel().getSelectedItem() + " ");
-                    msgField.requestFocus();
-                    msgField.positionCaret(msgField.getText().length() - 1);
-                }
-            }
-        );
-    }
+		clientsList.setOnMouseClicked(event -> {
+					if (event.getClickCount() == 2) {
+						msgField.clear();
+						msgField.setText("/w " + clientsList.getSelectionModel().getSelectedItem() + " ");
+						msgField.requestFocus();
+						msgField.positionCaret(msgField.getText().length() - 1);
+					}
+				}
+		);
+	}
 
-    private Stage createNicknameChanger() {
-        Stage stage = new Stage();
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/nickChanger.fxml"));
-            Parent root = fxmlLoader.load();
+	private Stage createNicknameChanger() {
+		Stage stage = new Stage();
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/nickChanger.fxml"));
+			Parent root = fxmlLoader.load();
 
-            stage.setTitle("Напишите свой новый ник");
-            stage.setScene(new Scene(root, 350, 250));
-            stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Напишите свой новый ник");
+			stage.setScene(new Scene(root, 350, 250));
+			stage.initModality(Modality.APPLICATION_MODAL);
 
-            nickChangeController = fxmlLoader.getController();
-            nickChangeController.setController(this);
+			nickChangeController = fxmlLoader.getController();
+			nickChangeController.setController(this);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return stage;
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return stage;
+	}
 
-    public void sendAuth() {
-        network.sendAuth(loginField.getText(), passField.getText());
-        loginField.clear();
-        passField.clear();
-    }
+	public void sendAuth() {
+		network.sendAuth(loginField.getText(), passField.getText());
+		loginField.clear();
+		passField.clear();
+	}
 
-    public void sendMsg() {
-        if (network.sendMsg(msgField.getText())) {
-            msgField.clear();
-            msgField.requestFocus();
-        }
-    }
+	public void sendMsg() {
+		if (network.sendMsg(msgField.getText())) {
+			msgField.clear();
+			msgField.requestFocus();
+		}
+	}
 
-    public void sendNewNick(String newNick) {
-        network.sendChangeNick(nickname, newNick);
-        nickname = newNick;
-        nickChangerStage.hide();
-    }
+	public void sendNewNick(String newNick) {
+		network.sendChangeNick(nickname, newNick);
+		renameHistoryFile(nickname, newNick);
+		nickname = newNick;
+		nickChangerStage.hide();
+	}
 
-    public void tryToReg(String login, String password, String nickname) {
-        network.sendReg(login, password, nickname);
-        regStage.hide();
-    }
+	public void tryToReg(String login, String password, String nickname) {
+		network.sendReg(login, password, nickname);
+		regStage.hide();
+	}
 
-    public void sendExit() {
-        network.sendMsg("/end");
-    }
+	public void sendExit() {
+		network.sendMsg("/end");
+	}
 
-    public void showAlert(String msg) {
-        Platform.runLater(() -> {
-            Alert alert = new Alert(Alert.AlertType.WARNING, msg, ButtonType.OK);
-            alert.showAndWait();
-        });
-    }
+	public void showAlert(String msg) {
+		Platform.runLater(() -> {
+			Alert alert = new Alert(Alert.AlertType.WARNING, msg, ButtonType.OK);
+			alert.showAndWait();
+		});
+	}
 
-    public void createNetwork() {
-        network = new Network();
-        network.setCallOnException(args -> showAlert(args[0]));
+	public void createNetwork() {
+		network = new Network();
+		network.setCallOnException(args -> showAlert(args[0]));
 
-        network.setCallOnCloseConnection(args -> setAuthenticated(false));
+		network.setCallOnCloseConnection(args -> setAuthenticated(false));
 
-        network.setCallOnAuthenticated(args -> {
-            setAuthenticated(true);
-            nickname = args[0];
-        });
+		network.setCallOnAuthenticated(args -> {
+			setAuthenticated(true);
+			nickname = args[0];
+			loadOrCreateHistoryFile();
+		});
 
-        network.setCallOnMsgReceived(args -> {
-            String msg = args[0];
-            if (msg.startsWith("/clients ")) {
-                String[] tokens = msg.split("\\s");
-                Platform.runLater(() -> {
-                    clientsList.getItems().clear();
-                    for (int i = 1; i < tokens.length; i++) {
-                        if (!nickname.equals(tokens[i])) {
-                            clientsList.getItems().add(tokens[i]);
-                        }
-                    }
-                });
-            } else if (msg.startsWith("/exception ")) {
-                showAlert(msg.substring(11));
-            } else {
-                textArea.appendText(msg + "\n");
-            }
-        });
-    }
+		network.setCallOnMsgReceived(args -> {
+			String msg = args[0];
+			if (msg.startsWith("/clients ")) {
+				String[] tokens = msg.split("\\s");
+				Platform.runLater(() -> {
+					clientsList.getItems().clear();
+					for (int i = 1; i < tokens.length; i++) {
+						if (!nickname.equals(tokens[i])) {
+							clientsList.getItems().add(tokens[i]);
+						}
+					}
+				});
+			} else if (msg.startsWith("/exception ")) {
+				showAlert(msg.substring(11));
+			} else {
+				textArea.appendText(msg + "\n");
+				addToHistory(msg);
+			}
+		});
+	}
 
-    public void showRegWindow(ActionEvent actionEvent) {
-        regStage.show();
-    }
+	public void showRegWindow(ActionEvent actionEvent) {
+		regStage.show();
+	}
 
-    public void showNickChangerWindow(ActionEvent actionEvent) {
-        nickChangerStage.show();
-    }
+	public void showNickChangerWindow(ActionEvent actionEvent) {
+		nickChangerStage.show();
+	}
 
-    private Stage createRegWindow() {
-        Stage stage = new Stage();
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/reg.fxml"));
-            Parent root = fxmlLoader.load();
+	private Stage createRegWindow() {
+		Stage stage = new Stage();
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/reg.fxml"));
+			Parent root = fxmlLoader.load();
 
-            stage.setTitle("Chat reg window");
-            stage.setScene(new Scene(root, 350, 250));
-            stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Chat reg window");
+			stage.setScene(new Scene(root, 350, 250));
+			stage.initModality(Modality.APPLICATION_MODAL);
 
-            regController = fxmlLoader.getController();
-            regController.setController(this);
+			regController = fxmlLoader.getController();
+			regController.setController(this);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return stage;
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return stage;
+	}
 
-    public void closeApp(ActionEvent actionEvent) {
-        this.sendExit();
-        Platform.exit();
-        System.exit(0);
-    }
+	public void closeApp(ActionEvent actionEvent) {
+		this.sendExit();
+		Platform.exit();
+		System.exit(0);
+	}
+
+	private void loadOrCreateHistoryFile() {
+		File history = new File("history_" + nickname + ".txt");
+
+		try {
+			if (history.exists()) {
+				FileReader reader = new FileReader(history);
+
+				BufferedReader br = new BufferedReader(reader);
+				long lineNumber = Files.lines(Paths.get("history_" + nickname + ".txt")).count();
+				long currentLineNumber = 0;
+				while (currentLineNumber <= (lineNumber - 100)) {
+					br.readLine();
+					++currentLineNumber;
+				}
+
+				String fileLine;
+				while ((fileLine = br.readLine()) != null) {
+					textArea.appendText(fileLine);
+					if (!fileLine.endsWith("\n")) {
+						textArea.appendText("\n");
+					}
+				}
+			} else {
+				history.createNewFile();
+			}
+		} catch (FileNotFoundException e) {
+			e.getMessage();
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.getMessage();
+			e.printStackTrace();
+		}
+	}
+
+	private void renameHistoryFile(String oldNick, String newNick) {
+		File oldHistory = new File("history_" + oldNick + ".txt");
+		File newHistory = new File("history_" + newNick + ".txt");
+
+		oldHistory.renameTo(newHistory);
+	}
+
+	private void addToHistory(String text) {
+		File history = new File("history_" + nickname + ".txt");
+
+		try {
+			if (history.exists()) {
+				FileOutputStream out = new FileOutputStream(history, true);
+				out.write(text.getBytes());
+
+				if (!text.endsWith("\n")) {
+					out.write("\n".getBytes());
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.getMessage();
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.getMessage();
+			e.printStackTrace();
+		}
+	}
 }
